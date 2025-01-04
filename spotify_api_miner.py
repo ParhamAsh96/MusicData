bb4fc45b1fdc05d5dd381838d2f402b5ba0b90ad
ParhamWolf3000 <parham.jobb@gmail.com>
2025-01-04 06:06:07 +0100

    spotify api test

--- spotify_api_miner.py
+++ spotify_api_miner.py
@@ -1,20 +1,20 @@
-import requests
-import json
+import requests, json, os
 
-service = "https://dit009-spotify-assignment.vercel.app/api/v1"
-url = f"{service}/artists/1HY2Jd0NmPuamShAr6KMms/albums"
+
+playlist_gym_hits = "artists/7dGJo4pcD2V6oG8kP0tJRR/albums"
+
+service = "https://dit009-spotify-assignment.vercel.app/api/v1/"
+url = f"{service}{playlist_gym_hits}"
 response = requests.get(url)
 data = response.json()
+print(data)
 
-for key, value in data.items():
-    print(f"{key} : {value}")
 
-'''
-def saving_json(data, filename="artist.json"):
-    with open(filename, "w") as file:
+# Save extracted data to JSON file
+def saving_json(data, filename="artist_albums"):
+    with open(f'MusicData/resources/{filename}.json', "w") as file:
         json.dump(data, file, indent=4)
 
-with open(data, "r") as json_file:
-    data =  json.load(json_file)
-    saving_json(data)
-'''
+saving_json(data)
+
+# os.remove(f'MusicData/resources/artist_albums.json')
