c681aa3d40dfe862b33f0e538970c0e8bdf5a1c8
ParhamWolf3000 <parham.jobb@gmail.com>
2025-01-08 02:21:12 +0100

    Progress 5.1

--- music_data_analyser.py
+++ music_data_analyser.py
@@ -1,4 +1,7 @@
-import json, pandas, matplotlib
+import json, matplotlib
+import pandas as pd
+from tabulate import tabulate
+from spotify_api_miner import file_path
 # from spotify_api_miner import read_json
 
 # By importing read_json, the program will send request and get the api live and save it as json in resources;
@@ -26,12 +29,13 @@ def main_menu(): # Add 'option' as a parameter at the end
     option = main_menu_options()
     match option:
         case 1:
+            chosen_artists_name, chosen_artists_id = choose_two_artists()
             submenu_option = submenu_option_one()
             match submenu_option:
                 case 1:
-                    compare_artists()
+                    compare_artists(chosen_artists_name, chosen_artists_id)
                 case 2:
-                    wikipedia_stats()
+                    wikipedia_stats(chosen_artists_name, chosen_artists_id)
                 case 3:
                     return
                 case 4:
@@ -161,8 +165,19 @@ def submenu_option_three():
             print(f"Something went wrong: {e}")
 
 
-def compare_artists():
-    pass
+def compare_artists(chosen_artists_name, chosen_artists_id):
+    total_albums_artist_one, total_singles_artist_one, total_albums_artist_two, total_singles_artist_two = parse_albums(chosen_artists_name, chosen_artists_id)
+    total_tracks_artist_one, total_tracks_artist_two = parse_tracks(chosen_artists_name, chosen_artists_id)
+
+    data = {
+        "Data": ["Albums", "Singles", "Tracks"],
+        chosen_artists_name[0] : [total_albums_artist_one, total_singles_artist_one, total_tracks_artist_one],
+        chosen_artists_name[1] : [total_albums_artist_two, total_singles_artist_two, total_tracks_artist_two]
+    }
+
+    data_table = pd.DataFrame(data)
+    data_table.set_index("Data", inplace=True)
+    print(tabulate(data, headers="keys", tablefmt="fancy_grid"))
 
 
 def wikipedia_stats():
@@ -205,77 +220,17 @@ def choose_two_artists():
         print(f"Something went wrong: {e}")
                         
 
-chosen_artists_name, chosen_artists_id = choose_two_artists()
-print(chosen_artists_name)
-print(chosen_artists_id)
-
-'''
-# The list of all artists and theirs ids that user can choose between.
-artists = []
-ids = []
-
-def create_list_of_artists(content):
-    for artist_name, artist_id in content.items():
-        artists.append(artist_name.casefold())
-        ids.append(artist_id)
-
-    return artists, ids
-'''
-#artists, ids = create_list_of_artists(content)
-
-# Using zip to creating a list of tuples from my lists artists and ids to 
-# make sure that thise informations never change in the program.     
-#for artist, id in zip(artists, ids):
-#    print(f"Artist: {artist}\n    ID: {id}\n")
-
-'''
-# OBS !!! I think i have to move it to miner.py and then import it here so that everything runs from analyser !!!!!
-# User choose his 2 favorites artists ofr getting analysis about
-def choose_artists():
-    chosen_artists_name = []
-    chosen_artists_id = []
-    try:
-        invalid_input = False
-        while not invalid_input and len(chosen_artists_name) < 2:
-            chosen_artist = input("Please choose two artists from the list above for comparing: ").casefold()
-            
-            if chosen_artist in chosen_artists_name:
-                print(f"{chosen_artist.capitalize()} is already chosen. Please choose another artis.")
-
-            elif chosen_artist not in artists:
-                print(f"The artist, {chosen_artist.capitalize()} that you've chosen is not in the list. Please choose an artist from the list.")
-        
-            elif chosen_artist in artists and chosen_artist not in chosen_artists_name:
-                index = artists.index(chosen_artist)
-                chosen_artists_name.append(chosen_artist)
-                chosen_artists_id.append(ids[index])
-                print(f"Your choice: {chosen_artist.capitalize()}")
-                invalid_input = True
-
-            if len(chosen_artists_name) < 2:
-                invalid_input = False
-                
-        print(f"Your first artist: {chosen_artists_name[0].upper()} \nYour second artist: {chosen_artists_name[1].upper()}")
-
-    except Exception as e:
-        print(f"Something went wrong: {e}.")
-
-    return chosen_artists_name, chosen_artists_id
-
-chosen_artists_name, chosen_artists_id =choose_artists()
-
-def read_chosen_json():
-    artist_one = read_json(f'MusicData/resources/{chosen_artists_name[0]}_{chosen_artists_id[0]}.json')
-    artist_two = read_json(f'MusicData/resources/{chosen_artists_name[1]}_{chosen_artists_id[1]}.json')
+def read_chosen_json(json_path, chosen_artists_name, chosen_artists_id):
+    artist_one = read_json(f'MusicData/resources/{json_path}/{chosen_artists_name[0]}_{chosen_artists_id[0]}.json')
+    artist_two = read_json(f'MusicData/resources/{json_path}/{chosen_artists_name[1]}_{chosen_artists_id[1]}.json')
 
     return artist_one, artist_two
-'''
-'''
-# Counts amount of the albums !!!
-def parse_albums():
-    artist_one, artist_two = read_chosen_json()
+
+
+def parse_albums(chosen_artists_name, chosen_artists_id):
+    artist_one, artist_two = read_chosen_json('albums', chosen_artists_name, chosen_artists_id)
     
-    # Analys number of albums from artist 1
+    # Analys number of albums and singles from artist 1
     total_albums_artist_one = []
     total_singles_artist_one = []
     for album in artist_one['items']:
@@ -284,9 +239,7 @@ def parse_albums():
         elif album['album_type'] == 'single':
             total_singles_artist_one.append(album['album_type'])
 
-    print(f"\n{chosen_artists_name[0].capitalize()} has total {len(total_albums_artist_one)} albums and {len(total_singles_artist_one)} singles.")
-
-    # Analys number of albums from artist 2
+    # Analys number of albums and singles from artist 2
     total_albums_artist_two = []
     total_singles_artist_two = []
     for album in artist_two['items']:
@@ -295,40 +248,33 @@ def parse_albums():
         elif album['album_type'] == 'single':
             total_singles_artist_two.append(album['album_type'])
 
-    print(f"\n{chosen_artists_name[1].capitalize()} has total {len(total_albums_artist_two)} albums and {len(total_singles_artist_two)} singles.")
+    return len(total_albums_artist_one), len(total_singles_artist_one), len(total_albums_artist_two), len(total_singles_artist_two)
 
-# Things i want user to choose:
-# JSON --> items --> [0] --> {total_tracks:x}
-#    print(artist_one['items'][0]['total_tracks'])
-def parse_tracks():
-    artist_one, artist_two = read_chosen_json()
 
+def parse_tracks(chosen_artists_name, chosen_artists_id):
+    artist_one, artist_two = read_chosen_json('albums', chosen_artists_name, chosen_artists_id)
+    
+    # Analys and calculate total number of tracks from artist 1
     total_tracks_artist_one = []
+
     for track in artist_one['items']:
         total_tracks_artist_one.append(track['total_tracks'])
-        #print(track['total_tracks'])
+
     for i in range(len(total_tracks_artist_one)):
         total_tracks_artist_one[i] = int(total_tracks_artist_one[i])
-        
-    print(f"\n{chosen_artists_name[0].capitalize()} has total {sum(total_tracks_artist_one)} tracks.")
-
+    
+    # Analys and calculate total number of tracks from artist 1
     total_tracks_artist_two = []
+
     for track in artist_two['items']:
         total_tracks_artist_two.append(track['total_tracks'])
-        #print(track['total_tracks'])
+
     for i in range(len(total_tracks_artist_two)):
         total_tracks_artist_two[i] = int(total_tracks_artist_two[i])
         
-    print(f"\n{chosen_artists_name[1].capitalize()} has total {sum(total_tracks_artist_two)} tracks.")
-'''
+    return sum(total_tracks_artist_one), sum(total_tracks_artist_two)
 
-#parse_albums()
-#parse_tracks()
 
-#def main():
-#    chosen_artists_name, chosen_artists_id = choose_artists()
-#    from spotify_api_miner import main as miner_main
-#    miner_main()
 
-#if __name__ == "__main__":
-#    main_menu()
+if __name__ == "__main__":
+    main_menu()
